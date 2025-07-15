e770b3ed65ae9e4bf401b1fa77aac07c9abae1a2
ModulabyAddsy <AddsyCloud@gmail.com>
2025-07-14 19:34:39 -0600

    Agregado FastAPI con Uvicorn para backend persistente

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ from cloud.setup_empresa_cloud import inicializar_estructura_sucursal
 app = FastAPI()
 
 @app.on_event("startup")
-def startup_event():
+async def startup_event():
     empresa_id = "PRUEBA_A004"
     sucursal_id = "SUC01"
     ruta = f"{empresa_id}/{sucursal_id}/bases/.keep"
     inicializar_estructura_sucursal(empresa_id, sucursal_id)
 
 @app.get("/")
-def home():
-    return {"mensaje": "Backend Modula activo en Render"}
-
-# Este bloque es útil para desarrollo local
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+def read_root():
+    return {"message": "Modula backend activo 🚀"}
